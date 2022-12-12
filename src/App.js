df61e94ffea7ad4bd258f0b3a0c42d189fe03eb6
kominsok <kominsok@naver.com>
2022-12-13 00:06:57 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+import react from 'react';
 import './App.css';
+import Gallery from './Gallery';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 style={{textAlign:'center'}}>고건우 Image Gallery</h1>
+      <h2 style={{textAlign:'center'}}>구독해 주세요!</h2>
+      <Gallery/>
+    </>
   );
 }
 
---
+++ src/Gallery.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import './gallery.css';
+import CloseIcon from '@material-ui/icons/Close';
+
+import Img1 from './img/img1.jpg';
+import Img2 from './img/img2.jpg';
+import Img3 from './img/img3.jpg';
+import Img4 from './img/img4.jpg';
+import Img5 from './img/img5.jpg';
+import Img6 from './img/img6.jpg';
+import Img7 from './img/img7.jpg';
+import Img8 from './img/img8.jpg';
+
+const Gallery = () => {
+    let data = [
+        {
+            id : 1,
+            imgSrc : Img1,
+        },
+        {
+            id : 2,
+            imgSrc : Img2,
+        },
+        {
+            id : 3,
+            imgSrc : Img3,
+        },
+        {
+            id : 4,
+            imgSrc : Img4,
+        },
+        {
+            id : 5,
+            imgSrc : Img5,
+        },
+        {
+            id : 6,
+            imgSrc : Img6,
+        },
+        {
+            id : 7,
+            imgSrc : Img7,
+        }
+        ,
+        {
+            id : 8,
+            imgSrc : Img8,
+        }
+    ]
+    const [model, setModel] = useState(false);
+    const [tempimgSrc, setTempImgSrc] = useState('');
+
+    const getImg=(imgSrc) =>{
+        setTempImgSrc(imgSrc);
+        setModel(true);
+    }
+
+    return(
+        <>
+        <div className={model? "model open" : "model"}>
+            <img src={tempimgSrc} />
+            <CloseIcon onClick={()=> setModel(false)}/>
+        </div>
+        <div className="gallery">
+            {data.map((item, index)=>{
+                return(
+                    <div className="pics" key={index} onClick={() => getImg(item.imgSrc)}>
+                        <img src={item.imgSrc} style={{width:'100%'}} />
+                    </div>
+                )
+            })}
+        </div>
+
+        </>
+    )
+}
+
+export default Gallery

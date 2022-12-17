db038858fc379e05f9749067d116c577168f4f2f
kominsok <kominsok@naver.com>
2022-12-17 10:59:31 +0900

    axios 적용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import react from 'react';
+import React from 'react';
 import './App.css';
 import Gallery from './Gallery';
+import axios from 'axios';
 
 function App() {
+
+  axios.get('http://localhost:8080/cdaccount').then(function(response){
+    console.log(response);
+  });
+
   return (
-    <>
+    <>  
       <h1 style={{textAlign:'center'}}>고건우 Image Gallery</h1>
       <h2 style={{textAlign:'center'}}>구독해 주세요!</h2>
       <Gallery/>
